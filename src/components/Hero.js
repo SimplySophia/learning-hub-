73610e49b13ed8b20602d5693e30b3faba5377dd
sophia vincent <sophire33@gmail.com>
2025-01-14 15:59:53 -0800

    hero component setup

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { ReactTyped } from 'react-typed';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='text-white'>
+        <div className='max-w-[800px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
+            <p className='text-[#00df9a] font-bold p-2 text-2xl'>Learn the latest skills with Brain</p>
+            <h1 className='md:text-5xl sm:text-4xl text-4xl font-bold md:py-6'>Up Your Skill With Brain.</h1>
+            <div className='flex justify-center items-center'>
+                <p className='md:text-4xl sm:text-2xl text-xl font-bold'>Professional Skills Available </p>
+                <ReactTyped
+                   className='md:text-4xl sm:text-2xl font-bold pl-2' 
+                   strings={['HTML', 'CSS', 'SAAS']} 
+                   typeSpeed={120} backSpeed={140} 
+                   loop 
+                />
+            </div>
+        </div>
+    </div>
   )
 }
 
